6478f9e13fc6e579deb6736592a21c8e014fc3cc
ibra-mh <112883985+ibra-mh@users.noreply.github.com>
2024-11-08 08:44:55 +0100

    Add files via upload

---
+++ Data/Migrations/20241027161720_UpdatedCustomerModifiedOn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerManagementSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedCustomerModifiedOn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "Customers");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomerManagementSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Fax { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+    }
+}
